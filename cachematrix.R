2a6aef3548f67987fe03ba5d4519a84745266217
Daryl Van Dyke <daryl_van_dyke@fws.gov>
2016-02-19 16:33:35 -0800

    done
    Yay!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## This function produces the cacheSolve-ready object and inits the NULL
 makeCacheMatrix <- function(x = matrix()) {
-
+        xp <- NULL
+        set <- function(y) {
+                x <<- y
+                xp <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) xp <<- inv
+        getinv <- function() xp
+        list(set = set, get = get,
+             setinv= setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## whereas this bad boy runs that method on the object
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        xp <- x$getinv()
+        if(!is.null(xp)) {
+                message("getting cached data")
+                return(xp)
+        }
+        data <- x$get()
+        xp <- solve(data, ...)
+        x$setinv(xp)
+        xp
 }
